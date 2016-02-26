66042c62a4d1c4922d57c079a74759675e4ab9f8
Quark Li <quarkli@gmail.com>
2016-02-27 00:58:10 +0800

    Initial commit for WebSocket Message Switch-Router

---
+++ config.rb
@@ -0,0 +1,9 @@
+#LOGFILE = "<path and filename to your log file>"
+#PRIVATEKEY = "<path and filename to your https server private key>",
+#CERTIFICATE = "<path and filename to your https server certificate>"
+#GEOFILE = "<path and filename to your geo location file>"
+
+LOGFILE = "/tmp/msg_srouter.log"
+PRIVATEKEY = "/home/ubuntu/node-https/gatherhub.key"
+CERTIFICATE = "/home/ubuntu/node-https/gatherhub.crt"
+GEOFILE = "/home/ubuntu/node-https/GeoLiteCity.dat"
