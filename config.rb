ae20212cef6e90c659ec031e9c84f5c387abd73e
Quark Li <qli@gatherhub.com>
2016-03-03 09:55:19 +0800

    Update config.rb

--- config.rb
+++ config.rb
@@ -3,7 +3,3 @@
 #CERTIFICATE = "<path and filename to your https server certificate>"
 #GEOFILE = "<path and filename to your geo location file>"
 
-LOGFILE = "/tmp/msg_srouter.log"
-PRIVATEKEY = "/home/ubuntu/node-https/gatherhub.key"
-CERTIFICATE = "/home/ubuntu/node-https/gatherhub.crt"
-GEOFILE = "/home/ubuntu/node-https/GeoLiteCity.dat"
